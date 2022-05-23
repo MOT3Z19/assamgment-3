3196d832e5ec9e68e5526a1640063e70c08ae6b9
Mot3z <motazboss2001@gmail.com>
2022-05-23 13:34:40 +0300

    Three Homwork

---
+++ lib/todo_app/data/tasks_data.dart
@@ -0,0 +1,11 @@
+import 'package:state_managment/todo_app/models/task_model.dart';
+
+List<Task> tasksList = [
+  Task('write assamgment'),
+   Task('learn new languge'),
+    Task('Studying',true),
+     Task('Reading',true),
+      Task('Eat Fruits'),
+       Task('play football',true),
+       Task('sleep' ),
+];
